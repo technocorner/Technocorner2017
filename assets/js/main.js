4d3626ebb6037d538bb992a90df65f048c6d78f4
rach.ep <rachmadpribadi@hotmail.com>
2016-11-16 23:07:01 +0700

    update gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -617,6 +617,7 @@ $(window).load(function(){
 		// init Isotope
 		var loading = 0;
 		var portfolioMsnry = $('#protfolio-msnry').isotope({
+			// filter: '.category-1',
 			itemSelector: '.single-port-item',
 			layoutMode: 'fitRows'
 		});
